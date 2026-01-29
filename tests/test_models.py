7067935f21e242f3ac219d7b4e739eea438bd992
rithika5656 <rithika0164@gmail.com>
2026-01-29 20:17:14 +0530

    test module

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,8 @@ from src.glass_classifier import models
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
+
 
 
 def test_create_knn():
@@ -23,4 +25,10 @@ def test_create_gb():
     model = models.create_gb_model(n_estimators=10)
     assert isinstance(model, GradientBoostingClassifier)
     assert model.n_estimators == 10
+
+def test_create_lr():
+    model = models.create_lr_model(max_iter=500)
+    assert isinstance(model, LogisticRegression)
+    assert model.max_iter == 500
+
 
