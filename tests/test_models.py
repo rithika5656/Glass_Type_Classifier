c2fc7fb6022d84843da746d742b54b75a5c7bc78
rithika5656 <rithika0164@gmail.com>
2026-01-29 20:13:32 +0530

    test module

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,8 @@
 from src.glass_classifier import models
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
 
 def test_create_knn():
     model = models.create_knn_model(n_neighbors=5)
@@ -11,3 +13,14 @@ def test_create_svm():
     model = models.create_svm_model(kernel='linear')
     assert isinstance(model, SVC)
     assert model.kernel == 'linear'
+
+def test_create_rf():
+    model = models.create_rf_model(n_estimators=10)
+    assert isinstance(model, RandomForestClassifier)
+    assert model.n_estimators == 10
+
+def test_create_gb():
+    model = models.create_gb_model(n_estimators=10)
+    assert isinstance(model, GradientBoostingClassifier)
+    assert model.n_estimators == 10
+
